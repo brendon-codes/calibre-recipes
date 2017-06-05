a22fe947a8d07feda404adccfdd0a89368f69793
Brendon <425026+brendon-codes@users.noreply.github.com>
2017-06-05 06:54:34 -0700

    Fixes patches

--- installer/install.bash
+++ installer/install.bash
@@ -13,5 +13,4 @@ sudo python2 ./custom_installer.py
 ##
 ## Patch some buggy code
 ##
-sudo mv /usr/local/opt/calibre/lib/python2.7/site-packages/cssutils/css/value.pyo  /usr/local/opt/calibre/lib/python2.7/site-packages/cssutils/css/value_orig.pyo
-sudo cp ./patches/cssutils/css/value.py /usr/local/opt/calibre/lib/python2.7/site-packages/cssutils/css/value.py
+sudo bash ./apply_patches.bash
