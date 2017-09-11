f20e98224ed43e8f779b7f1684c6a5fcc947735c
Brendon <425026+brendon-codes@users.noreply.github.com>
2017-09-11 10:43:34 -0700

    Fixes to HN

---
+++ etlds/conv.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+from pprint import pprint
+
+
+def clean(lin):
+    a = lin.strip()
+    parts = a.split(".")
+    red = filter(lambda r: r != "*", parts)
+    b = ".".join(red)
+    return b
+
+
+def main():
+    items = list(map(clean, sys.stdin))
+    tree = json.dumps(items, indent=4, sort_keys=True)
+    froze = "".join(["DOMAINS = frozenset(\n", tree, "\n)"])
+    print(froze)
+    return True
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
