2269b026885efc028b196a5cd697d07587f758de
Brendon <425026+brendon-codes@users.noreply.github.com>
2017-09-14 12:32:27 -0700

    More working on getting HN to work

--- installer/patches/scripts/value_py.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-FILEDIR="/usr/local/opt/calibre/lib/python2.7/site-packages/cssutils/css"
-wget https://bitbucket.org/cthedot/cssutils/raw/default/src/cssutils/css/value.py -O "patches/builds/value.py"
-patch "patches/builds/value.py" "patches/diffs/value_py.patch"
-sudo mv "${FILEDIR}/value.pyo" "${FILEDIR}/value_orig.pyo"
-sudo mv "patches/builds/value.py" "${FILEDIR}/value.py"
