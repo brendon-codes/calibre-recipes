ccea413f13f049d27161d60a95177207c113a0bd
Brendon <425026+brendon-codes@users.noreply.github.com>
2017-09-21 10:11:20 -0700

    Improves domain checking code

--- etlds/conv.py
+++ etlds/conv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 import json
 from pprint import pprint
@@ -16,7 +17,18 @@ def clean(lin):
 def main():
     items = list(map(clean, sys.stdin))
     tree = json.dumps(items, indent=None, sort_keys=True)
-    froze = "".join(["DOMAINS = frozenset(\n", tree, "\n)"])
+    ##
+    ## Add unicode labels to each entry so this can be included in python2
+    ## scripts.
+    ##
+    tree_uni = (
+        re.sub(
+            r"(?u)(?P<val>\u0022[^\u0022\s]+\u0022)",
+            r"ur\g<val>",
+            tree
+        )
+    )
+    froze = "".join(["DOMAINS = frozenset(\n", tree_uni, "\n)"])
     print(froze)
     return True
 
